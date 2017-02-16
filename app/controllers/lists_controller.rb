17ba47d75046f3833e7f7befab2c6790a4844dfa
ujnujn1980 <ujnujn1980@gmail.com>
2017-02-17 08:15:32 +0900

    add before_action: authenticate user to item&user controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /lists
   # GET /lists.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  root to: "home#index"
+ 
   resources :items
   resources :lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/done/:id', to:  'items#done', as: 'done'
-  root to: "lists#index"
+  #root to: "lists#index"
 end
