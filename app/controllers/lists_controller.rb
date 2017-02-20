33cee38bb60c12980783921ec78b10c0430207ae
ujnujn1980 <ujnujn1980@gmail.com>
2017-02-20 23:26:10 +0900

    app/views/layouts/application.html.erb notice comment outed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,8 +34,10 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
-        format.json { render :show, status: :created, location: @list }
+        #format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.html { redirect_to lists_path, notice: 'List was successfully created.' }
+        #format.html { redirect_to lists_path }
+        #format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
         format.json { render json: @list.errors, status: :unprocessable_entity }
