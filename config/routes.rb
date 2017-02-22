f0c66bce323491c7935562ad9c7b04439df0f697
ujnujn1980 <ujnujn1980@gmail.com>
2017-02-22 22:34:10 +0900

    change @list to lists_path on lists_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
+  get      'home/index'
   root to: "home#index"
  
   #resources :lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/done/:id', to:  'items#done', as: 'done'
-  #root to: "lists#index"
-  #get '/patients/:id', to: 'items#show'
-  get '/patients/:id', to: 'items#show', as: 'patient'
-  get '/items/new/:id', to: 'items#new' , as: 'new_item'
-  get '/list/:id', to: 'items#index' , as: 'list_items'
-  post '/list/:id', to: 'items#create' 
+  get  '/done/:id',      to: 'items#done',   as: 'done'
+  get  '/patients/:id',  to: 'items#show',   as: 'patient'
+  get  '/items/new/:id', to: 'items#new' ,   as: 'new_item'
+  get  '/list/:id',      to: 'items#index' , as: 'list_items'
+  post '/list/:id',      to: 'items#create' 
 
   resources :items, except: [:new]
   resources :lists 
